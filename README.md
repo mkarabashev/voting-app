9eeb7bd2f7eb22281557288e6721816bb2c11f28
Maxim <mkarabashev@gmail.com>
2017-02-06 02:41:57 +0200

    readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+## This app is still in development. I've so far set up linting, testing and dev + prod servers
+## TODO
+### isomorphic-rendering
+### websockets
+### facebook authentication
+### rest of the app
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+'use strict';
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 8080;
+const PORT = parseInt(process.env.PORT) || 8080;
 
-app().listen(3002, () => console.log(`Process ${process.pid} listening on port ${PORT}`));
+app().listen(PORT, () => console.log(`Process ${process.pid} listening on port ${PORT}`));
